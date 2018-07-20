436bb8353d485024049367135d53f87207925ff2
nonop1507 <nono1507@naver.com>
2018-07-21 01:43:54 +0900

    Setting

--- app/src/main/java/rankhep/com/ddaal/adapter/MainTabPagerAdapter.kt
+++ app/src/main/java/rankhep/com/ddaal/adapter/MainTabPagerAdapter.kt
@@ -5,6 +5,7 @@ import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentStatePagerAdapter
 import rankhep.com.ddaal.fragment.JobSearchFragment
 import rankhep.com.ddaal.fragment.LawyerFragment
+import rankhep.com.ddaal.fragment.SettingFragment
 
 class MainTabPagerAdapter(fm:FragmentManager): FragmentStatePagerAdapter(fm) {
     override fun getItem(position: Int): Fragment {
@@ -14,6 +15,9 @@ class MainTabPagerAdapter(fm:FragmentManager): FragmentStatePagerAdapter(fm) {
             }
             1->{
                 return LawyerFragment()
+            }
+            2->{
+                return SettingFragment()
             }
         }
         return JobSearchFragment()
--- app/src/main/java/rankhep/com/ddaal/fragment/SettingFragment.kt
+++ app/src/main/java/rankhep/com/ddaal/fragment/SettingFragment.kt
@@ -1,4 +1,16 @@
 package rankhep.com.ddaal.fragment
 
-class SettingFragment {
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import rankhep.com.ddaal.R
+
+class SettingFragment: Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = LayoutInflater.from(context).inflate(R.layout.fragment_setting, null)
+
+        return view
+    }
 }
